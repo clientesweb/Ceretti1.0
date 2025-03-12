afab5c5f7a6dbb1d0bad005dc7275662a2c0e403
clientesweb <alexisifran32@gmail.com>
2025-03-12 18:51:18 -0300

    Actualizar page.tsx

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,10 +1,17 @@
-import BreadcrumbShop from "@/components/shop-page/BreadcrumbShop"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import MobileFilters from "@/components/shop-page/filters/MobileFilters"
-import Filters from "@/components/shop-page/filters"
-import { FiSliders } from "react-icons/fi"
-import { newArrivalsData, relatedProductData } from "../page"
-import ProductCard from "@/components/common/ProductCard"
+import BreadcrumbShop from "@/components/shop-page/BreadcrumbShop";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import MobileFilters from "@/components/shop-page/filters/MobileFilters";
+import Filters from "@/components/shop-page/filters";
+import { FiSliders } from "react-icons/fi";
+import { newArrivalsData, relatedProductData, topSellingData } from "../page";
+import ProductCard from "@/components/common/ProductCard";
 import {
   Pagination,
   PaginationContent,
@@ -13,7 +20,7 @@ import {
   PaginationLink,
   PaginationNext,
   PaginationPrevious,
-} from "@/components/ui/pagination"
+} from "@/components/ui/pagination";
 
 export default function ShopPage() {
   return (
@@ -36,7 +43,9 @@ export default function ShopPage() {
                 <MobileFilters />
               </div>
               <div className="flex flex-col sm:items-center sm:flex-row">
-                <span className="text-sm md:text-base text-black/60 mr-3">Showing 1-10 of 100 Products</span>
+                <span className="text-sm md:text-base text-black/60 mr-3">
+                  Showing 1-10 of 100 Products
+                </span>
                 <div className="flex items-center">
                   Sort by:{" "}
                   <Select defaultValue="most-popular">
@@ -53,7 +62,11 @@ export default function ShopPage() {
               </div>
             </div>
             <div className="w-full grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-5">
-              {[...relatedProductData.slice(1, 4), ...newArrivalsData.slice(1, 4)].map((product) => (
+              {[
+                ...relatedProductData.slice(1, 4),
+                ...newArrivalsData.slice(1, 4),
+                ...topSellingData.slice(1, 4),
+              ].map((product) => (
                 <ProductCard key={product.id} data={product} />
               ))}
             </div>
@@ -62,17 +75,27 @@ export default function ShopPage() {
               <PaginationPrevious href="#" className="border border-black/10" />
               <PaginationContent>
                 <PaginationItem>
-                  <PaginationLink href="#" className="text-black/50 font-medium text-sm" isActive>
+                  <PaginationLink
+                    href="#"
+                    className="text-black/50 font-medium text-sm"
+                    isActive
+                  >
                     1
                   </PaginationLink>
                 </PaginationItem>
                 <PaginationItem>
-                  <PaginationLink href="#" className="text-black/50 font-medium text-sm">
+                  <PaginationLink
+                    href="#"
+                    className="text-black/50 font-medium text-sm"
+                  >
                     2
                   </PaginationLink>
                 </PaginationItem>
                 <PaginationItem className="hidden lg:block">
-                  <PaginationLink href="#" className="text-black/50 font-medium text-sm">
+                  <PaginationLink
+                    href="#"
+                    className="text-black/50 font-medium text-sm"
+                  >
                     3
                   </PaginationLink>
                 </PaginationItem>
@@ -80,17 +103,26 @@ export default function ShopPage() {
                   <PaginationEllipsis className="text-black/50 font-medium text-sm" />
                 </PaginationItem>
                 <PaginationItem className="hidden lg:block">
-                  <PaginationLink href="#" className="text-black/50 font-medium text-sm">
+                  <PaginationLink
+                    href="#"
+                    className="text-black/50 font-medium text-sm"
+                  >
                     8
                   </PaginationLink>
                 </PaginationItem>
                 <PaginationItem className="hidden sm:block">
-                  <PaginationLink href="#" className="text-black/50 font-medium text-sm">
+                  <PaginationLink
+                    href="#"
+                    className="text-black/50 font-medium text-sm"
+                  >
                     9
                   </PaginationLink>
                 </PaginationItem>
                 <PaginationItem>
-                  <PaginationLink href="#" className="text-black/50 font-medium text-sm">
+                  <PaginationLink
+                    href="#"
+                    className="text-black/50 font-medium text-sm"
+                  >
                     10
                   </PaginationLink>
                 </PaginationItem>
@@ -102,6 +134,5 @@ export default function ShopPage() {
         </div>
       </div>
     </main>
-  )
+  );
 }
-
