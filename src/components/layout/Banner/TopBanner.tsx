f0adfe3fb9dbdc712757c603c9df6e1f58820c14
clientesweb <alexisifran32@gmail.com>
2025-03-12 17:33:57 -0300

    Actualizar TopBanner.tsx

--- src/components/layout/Banner/TopBanner.tsx
+++ src/components/layout/Banner/TopBanner.tsx
@@ -1,16 +1,15 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+import Link from "next/link"
 
 const TopBanner = () => {
   return (
-    <div className="bg-black text-white text-center py-2 px-2 sm:px-4 xl:px-0">
+    <div className="bg-ceretti-blue text-white text-center py-2 px-2 sm:px-4 xl:px-0">
       <div className="relative max-w-frame mx-auto">
         <p className="text-xs sm:text-sm">
-          Sign up and get 20% off to your first order.{" "}
+          ¡No esperes más para destacar en redes sociales!{" "}
           <Link href="#" className="underline font-medium">
-            Sign Up Now
+            Regístrate ahora
           </Link>
         </p>
         <Button
@@ -18,19 +17,14 @@ const TopBanner = () => {
           className="hover:bg-transparent absolute right-0 top-1/2 -translate-y-1/2 w-fit h-fit p-1 hidden sm:flex"
           size="icon"
           type="button"
-          aria-label="close banner"
+          aria-label="cerrar banner"
         >
-          <Image
-            priority
-            src="/icons/times.svg"
-            height={13}
-            width={13}
-            alt="close banner"
-          />
+          <Image priority src="/icons/times.svg" height={13} width={13} alt="cerrar banner" />
         </Button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default TopBanner;
+export default TopBanner
+
