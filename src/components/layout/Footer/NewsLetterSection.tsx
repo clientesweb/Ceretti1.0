76b9b5eefbf53538ecaad5ea4b384fffb61a6539
clientesweb <alexisifran32@gmail.com>
2025-03-12 17:38:08 -0300

    Actualizar NewsLetterSection.tsx

--- src/components/layout/Footer/NewsLetterSection.tsx
+++ src/components/layout/Footer/NewsLetterSection.tsx
@@ -1,20 +1,14 @@
-import { Button } from "@/components/ui/button";
-import InputGroup from "@/components/ui/input-group";
-import { cn } from "@/lib/utils";
-import { integralCF } from "@/styles/fonts";
-import Image from "next/image";
-import React from "react";
+import { Button } from "@/components/ui/button"
+import InputGroup from "@/components/ui/input-group"
+import { cn } from "@/lib/utils"
+import { integralCF } from "@/styles/fonts"
+import Image from "next/image"
 
 const NewsLetterSection = () => {
   return (
-    <div className="relative grid grid-cols-1 md:grid-cols-2 py-9 md:py-11 px-6 md:px-16 max-w-frame mx-auto bg-black rounded-[20px]">
-      <p
-        className={cn([
-          integralCF.className,
-          "font-bold text-[32px] md:text-[40px] text-white mb-9 md:mb-0",
-        ])}
-      >
-        STAY UP TO DATE ABOUT OUR LATEST OFFERS
+    <div className="relative grid grid-cols-1 md:grid-cols-2 py-9 md:py-11 px-6 md:px-16 max-w-frame mx-auto bg-ceretti-blue rounded-[20px]">
+      <p className={cn([integralCF.className, "font-bold text-[32px] md:text-[40px] text-white mb-9 md:mb-0"])}>
+        MANTENTE AL DÍA CON NUESTRAS ÚLTIMAS OFERTAS
       </p>
       <div className="flex items-center">
         <div className="flex flex-col w-full max-w-[349px] mx-auto">
@@ -32,22 +26,23 @@ const NewsLetterSection = () => {
             <InputGroup.Input
               type="email"
               name="email"
-              placeholder="Enter your email address"
+              placeholder="Ingresa tu dirección de email"
               className="bg-transparent placeholder:text-black/40 placeholder:text-sm sm:placeholder:text-base"
             />
           </InputGroup>
           <Button
             variant="secondary"
             className="text-sm sm:text-base font-medium bg-white h-12 rounded-full px-4 py-3"
-            aria-label="Subscribe to Newsletter"
+            aria-label="Suscribirse al boletín"
             type="button"
           >
-            Subscribe to Newsletter
+            Suscribirse al Boletín
           </Button>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default NewsLetterSection;
+export default NewsLetterSection
+
