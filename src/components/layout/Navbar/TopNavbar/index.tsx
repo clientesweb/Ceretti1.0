8a3c3fe976d501b8632da1d69ad0157fa44cd433
clientesweb <alexisifran32@gmail.com>
2025-03-12 17:34:34 -0300

    Actualizar index.tsx

--- src/components/layout/Navbar/TopNavbar/index.tsx
+++ src/components/layout/Navbar/TopNavbar/index.tsx
@@ -1,73 +1,70 @@
-import { cn } from "@/lib/utils";
-import { integralCF } from "@/styles/fonts";
-import Link from "next/link";
-import React from "react";
-import { NavMenu } from "../navbar.types";
-import { MenuList } from "./MenuList";
-import {
-  NavigationMenu,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu";
-import { MenuItem } from "./MenuItem";
-import Image from "next/image";
-import InputGroup from "@/components/ui/input-group";
-import ResTopNavbar from "./ResTopNavbar";
-import CartBtn from "./CartBtn";
+import { cn } from "@/lib/utils"
+import { integralCF } from "@/styles/fonts"
+import Link from "next/link"
+import React from "react"
+import type { NavMenu } from "../navbar.types"
+import { MenuList } from "./MenuList"
+import { NavigationMenu, NavigationMenuList } from "@/components/ui/navigation-menu"
+import { MenuItem } from "./MenuItem"
+import Image from "next/image"
+import InputGroup from "@/components/ui/input-group"
+import ResTopNavbar from "./ResTopNavbar"
+import CartBtn from "./CartBtn"
 
 const data: NavMenu = [
   {
     id: 1,
-    label: "Shop",
+    label: "Tienda",
     type: "MenuList",
     children: [
       {
         id: 11,
-        label: "Men's clothes",
-        url: "/shop#men-clothes",
-        description: "In attractive and spectacular colors and designs",
+        label: "Redes Sociales",
+        url: "/shop#redes-sociales",
+        description: "Servicios para Instagram, Facebook, Twitter y más",
       },
       {
         id: 12,
-        label: "Women's clothes",
-        url: "/shop#women-clothes",
-        description: "Ladies, your style and tastes are important to us",
+        label: "Paquetes",
+        url: "/shop#paquetes",
+        description: "Combos especiales para potenciar tu presencia digital",
       },
       {
         id: 13,
-        label: "Kids clothes",
-        url: "/shop#kids-clothes",
-        description: "For all ages, with happy and beautiful colors",
+        label: "Servicios Premium",
+        url: "/shop#premium",
+        description: "Soluciones personalizadas para influencers y marcas",
       },
       {
         id: 14,
-        label: "Bags and Shoes",
-        url: "/shop#bag-shoes",
-        description: "Suitable for men, women and all tastes and styles",
+        label: "Ofertas",
+        url: "/shop#ofertas",
+        description: "Promociones especiales por tiempo limitado",
       },
     ],
   },
   {
     id: 2,
     type: "MenuItem",
-    label: "On Sale",
-    url: "/shop#on-sale",
+    label: "Ofertas",
+    url: "/shop#ofertas",
     children: [],
   },
   {
     id: 3,
     type: "MenuItem",
-    label: "New Arrivals",
-    url: "/shop#new-arrivals",
+    label: "Novedades",
+    url: "/shop#novedades",
     children: [],
   },
   {
     id: 4,
     type: "MenuItem",
-    label: "Brands",
-    url: "/shop#brands",
+    label: "Servicios",
+    url: "/shop#servicios",
     children: [],
   },
-];
+]
 
 const TopNavbar = () => {
   return (
@@ -79,43 +76,30 @@ const TopNavbar = () => {
           </div>
           <Link
             href="/"
-            className={cn([
-              integralCF.className,
-              "text-2xl lg:text-[32px] mb-2 mr-3 lg:mr-10",
-            ])}
+            className={cn([integralCF.className, "text-2xl lg:text-[32px] mb-2 mr-3 lg:mr-10 flex items-center"])}
           >
-            SHOP.CO
+            <span className="mr-2">TT</span>
+            <span className="text-sm font-normal">CERETTI MGTM</span>
           </Link>
         </div>
         <NavigationMenu className="hidden md:flex mr-2 lg:mr-7">
           <NavigationMenuList>
             {data.map((item) => (
               <React.Fragment key={item.id}>
-                {item.type === "MenuItem" && (
-                  <MenuItem label={item.label} url={item.url} />
-                )}
-                {item.type === "MenuList" && (
-                  <MenuList data={item.children} label={item.label} />
-                )}
+                {item.type === "MenuItem" && <MenuItem label={item.label} url={item.url} />}
+                {item.type === "MenuList" && <MenuList data={item.children} label={item.label} />}
               </React.Fragment>
             ))}
           </NavigationMenuList>
         </NavigationMenu>
         <InputGroup className="hidden md:flex bg-[#F0F0F0] mr-3 lg:mr-10">
           <InputGroup.Text>
-            <Image
-              priority
-              src="/icons/search.svg"
-              height={20}
-              width={20}
-              alt="search"
-              className="min-w-5 min-h-5"
-            />
+            <Image priority src="/icons/search.svg" height={20} width={20} alt="buscar" className="min-w-5 min-h-5" />
           </InputGroup.Text>
           <InputGroup.Input
             type="search"
             name="search"
-            placeholder="Search for products..."
+            placeholder="Buscar servicios..."
             className="bg-transparent placeholder:text-black/40"
           />
         </InputGroup>
@@ -126,7 +110,7 @@ const TopNavbar = () => {
               src="/icons/search-black.svg"
               height={100}
               width={100}
-              alt="search"
+              alt="buscar"
               className="max-w-[22px] max-h-[22px]"
             />
           </Link>
@@ -137,14 +121,15 @@ const TopNavbar = () => {
               src="/icons/user.svg"
               height={100}
               width={100}
-              alt="user"
+              alt="usuario"
               className="max-w-[22px] max-h-[22px]"
             />
           </Link>
         </div>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default TopNavbar;
+export default TopNavbar
+
