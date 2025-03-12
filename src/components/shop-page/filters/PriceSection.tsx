c1fda73c542ba0fbd76d9a9a7ad873a0b429f954
clientesweb <alexisifran32@gmail.com>
2025-03-12 20:05:59 -0300

    Actualizar PriceSection.tsx

--- src/components/shop-page/filters/PriceSection.tsx
+++ src/components/shop-page/filters/PriceSection.tsx
@@ -1,32 +1,25 @@
-import React from "react";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { Slider } from "@/components/ui/slider";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
+import { Slider } from "@/components/ui/slider"
 
 const PriceSection = () => {
   return (
     <Accordion type="single" collapsible defaultValue="filter-price">
       <AccordionItem value="filter-price" className="border-none">
         <AccordionTrigger className="text-black font-bold text-xl hover:no-underline p-0 py-0.5">
-          Price
+          Precio
         </AccordionTrigger>
         <AccordionContent className="pt-4" contentClassName="overflow-visible">
-          <Slider
-            defaultValue={[50, 200]}
-            min={0}
-            max={250}
-            step={1}
-            label="$"
-          />
+          <Slider defaultValue={[50, 200]} min={0} max={250} step={1} label="$" />
+          <div className="flex justify-between mt-4 text-sm text-black/60">
+            <span>$0</span>
+            <span>$250</span>
+          </div>
           <div className="mb-3" />
         </AccordionContent>
       </AccordionItem>
     </Accordion>
-  );
-};
+  )
+}
 
-export default PriceSection;
+export default PriceSection
+
