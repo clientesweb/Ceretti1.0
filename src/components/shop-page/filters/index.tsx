4943f553df048db049f2c3981c2abffb60cdd73b
clientesweb <alexisifran32@gmail.com>
2025-03-12 20:05:18 -0300

    Actualizar index.tsx

--- src/components/shop-page/filters/index.tsx
+++ src/components/shop-page/filters/index.tsx
@@ -1,10 +1,6 @@
-import React from "react";
-import CategoriesSection from "@/components/shop-page/filters/CategoriesSection";
-import ColorsSection from "@/components/shop-page/filters/ColorsSection";
-import DressStyleSection from "@/components/shop-page/filters/DressStyleSection";
-import PriceSection from "@/components/shop-page/filters/PriceSection";
-import SizeSection from "@/components/shop-page/filters/SizeSection";
-import { Button } from "@/components/ui/button";
+import CategoriesSection from "@/components/shop-page/filters/CategoriesSection"
+import PriceSection from "@/components/shop-page/filters/PriceSection"
+import { Button } from "@/components/ui/button"
 
 const Filters = () => {
   return (
@@ -14,19 +10,12 @@ const Filters = () => {
       <hr className="border-t-black/10" />
       <PriceSection />
       <hr className="border-t-black/10" />
-      <ColorsSection />
-      <hr className="border-t-black/10" />
-      <SizeSection />
-      <hr className="border-t-black/10" />
-      <DressStyleSection />
-      <Button
-        type="button"
-        className="bg-black w-full rounded-full text-sm font-medium py-4 h-12"
-      >
-        Apply Filter
+      <Button type="button" className="bg-ceretti-blue w-full rounded-full text-sm font-medium py-4 h-12">
+        Aplicar Filtros
       </Button>
     </>
-  );
-};
+  )
+}
 
-export default Filters;
+export default Filters
+
