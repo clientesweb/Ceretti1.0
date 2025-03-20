5dd36ff4324c8a851c0a2ad217b17a71518dcdc6
clientesweb <alexisifran32@gmail.com>
2025-03-20 00:19:43 -0300

    Actualizar ProductCard.tsx

--- src/components/cart-page/ProductCard.tsx
+++ src/components/cart-page/ProductCard.tsx
@@ -21,7 +21,7 @@ const ProductCard = ({ data }: ProductCardProps) => {
         className="bg-[#F0EEED] rounded-lg w-full min-w-[100px] max-w-[100px] sm:max-w-[124px] aspect-square overflow-hidden"
       >
         <Image
-          src={data.srcUrl}
+          src={data.srcUrl || "/placeholder.svg"}
           width={124}
           height={124}
           className="rounded-md w-full h-full object-cover hover:scale-110 transition-all duration-500"
@@ -66,20 +66,20 @@ const ProductCard = ({ data }: ProductCardProps) => {
           <div className="flex items-center space-x-[5px] xl:space-x-2.5">
             {data.discount.percentage > 0 ? (
               <span className="font-bold text-black text-xl xl:text-2xl">
-                {`$${Math.round(data.price - (data.price * data.discount.percentage) / 100)}`}
+                {`$${Math.round(data.price - (data.price * data.discount.percentage) / 100)} ARS`}
               </span>
             ) : data.discount.amount > 0 ? (
               <span className="font-bold text-black text-xl xl:text-2xl">
-                {`$${data.price - data.discount.amount}`}
+                {`$${data.price - data.discount.amount} ARS`}
               </span>
             ) : (
-              <span className="font-bold text-black text-xl xl:text-2xl">${data.price}</span>
+              <span className="font-bold text-black text-xl xl:text-2xl">${data.price} ARS</span>
             )}
             {data.discount.percentage > 0 && (
-              <span className="font-bold text-black/40 line-through text-xl xl:text-2xl">${data.price}</span>
+              <span className="font-bold text-black/40 line-through text-xl xl:text-2xl">${data.price} ARS</span>
             )}
             {data.discount.amount > 0 && (
-              <span className="font-bold text-black/40 line-through text-xl xl:text-2xl">${data.price}</span>
+              <span className="font-bold text-black/40 line-through text-xl xl:text-2xl">${data.price} ARS</span>
             )}
             {data.discount.percentage > 0 ? (
               <span className="font-medium text-[10px] xl:text-xs py-1.5 px-3.5 rounded-full bg-[#FF3333]/10 text-[#FF3333]">
@@ -88,7 +88,7 @@ const ProductCard = ({ data }: ProductCardProps) => {
             ) : (
               data.discount.amount > 0 && (
                 <span className="font-medium text-[10px] xl:text-xs py-1.5 px-3.5 rounded-full bg-[#FF3333]/10 text-[#FF3333]">
-                  {`-$${data.discount.amount}`}
+                  {`-$${data.discount.amount} ARS`}
                 </span>
               )
             )}
