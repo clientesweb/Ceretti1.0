ff0e9591d15bb17282ce7a6758d156ed3d546a68
clientesweb <alexisifran32@gmail.com>
2025-03-19 22:40:21 -0300

    Actualizar utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,17 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
-export const compareArrays = (a: any[], b: any[]) => {
-  return a.toString() === b.toString();
-};
+export function compareArrays(a: any[], b: any[]) {
+  return a.length === b.length && a.every((element, index) => element === b[index])
+}
+
+// Función para buscar todos los archivos de rutas dinámicas
+export function findDynamicRoutes() {
+  // Esta función es solo para depuración y no se usa en producción
+  return "Función para depuración"
+}
+
