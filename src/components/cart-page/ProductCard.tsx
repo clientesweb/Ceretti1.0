5b7990fa4cbd4832c1a1e77d16a832069a27ac53
clientesweb <alexisifran32@gmail.com>
2025-03-20 00:48:41 -0300

    Actualizar ProductCard.tsx

--- src/components/cart-page/ProductCard.tsx
+++ src/components/cart-page/ProductCard.tsx
@@ -17,7 +17,11 @@ const ProductCard = ({ data }: ProductCardProps) => {
   return (
     <div className="flex items-start space-x-4">
       <Link
-        href={`/shop/product/${data.id}/${data.name.split(" ").join("-")}`}
+        href={
+          data.id === 1
+            ? `/shop/product/1/seguidores-instagram-premium`
+            : `/shop/product/${data.id}/${data.name.split(" ").join("-")}`
+        }
         className="bg-[#F0EEED] rounded-lg w-full min-w-[100px] max-w-[100px] sm:max-w-[124px] aspect-square overflow-hidden"
       >
         <Image
@@ -32,7 +36,11 @@ const ProductCard = ({ data }: ProductCardProps) => {
       <div className="flex w-full self-stretch flex-col">
         <div className="flex items-center justify-between">
           <Link
-            href={`/shop/product/${data.id}/${data.name.split(" ").join("-")}`}
+            href={
+              data.id === 1
+                ? `/shop/product/1/seguidores-instagram-premium`
+                : `/shop/product/${data.id}/${data.name.split(" ").join("-")}`
+            }
             className="text-black font-bold text-base xl:text-xl"
           >
             {data.name}
