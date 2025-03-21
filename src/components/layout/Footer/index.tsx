66beddd036cbbd7dcdcf3faa830fd9e5b5d5ea42
clientesweb <alexisifran32@gmail.com>
2025-03-21 19:50:06 -0300

    Actualizar index.tsx

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -69,7 +69,7 @@ const Footer = () => {
             <div className="flex flex-col lg:col-span-3 lg:max-w-[248px]">
               <h1 className={cn([integralCF.className, "text-[28px] lg:text-[32px] mb-6 flex items-center"])}>
                 <span className="mr-2">TT</span>
-                <span className="text-sm font-normal">CERETTI MGTM</span>
+                <span className="text-sm font-normal">CERETTI MGMT</span>
               </h1>
               <p className="text-black/60 text-sm mb-9">
                 Tenemos servicios que se adaptan a tus necesidades y de los que estarás orgulloso. Desde Instagram hasta
@@ -98,7 +98,7 @@ const Footer = () => {
           <hr className="h-[1px] border-t-black/10 mb-6" />
           <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center mb-2">
             <p className="text-sm text-center sm:text-left text-black/60 mb-4 sm:mb-0 sm:mr-1">
-              CERETTI MGTM © 2023 - Todos los derechos reservados
+              CERETTI MGMT © 2023 - Todos los derechos reservados
             </p>
             <p className="text-sm text-center sm:text-left text-black/60 mb-4 sm:mb-0">
               Desarrollado por{" "}
