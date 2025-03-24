811f74d73c0f4ba3fa6ebeb8e5a14a47293758f8
clientesweb <alexisifran32@gmail.com>
2025-03-24 13:30:10 -0300

    Actualizar toast.tsx

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -31,8 +31,6 @@ const toastVariants = cva(
       variant: {
         default: "border bg-background text-foreground",
         destructive: "destructive group border-destructive bg-destructive text-destructive-foreground",
-        success: "border-green-500 bg-green-500 text-white",
-        ceretti: "border-ceretti-blue bg-ceretti-blue text-white",
       },
     },
     defaultVariants: {
@@ -113,4 +111,3 @@ export {
   ToastClose,
   ToastAction,
 }
-
