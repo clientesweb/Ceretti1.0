379b52ab8e69cb7b74d2693b2900b6af5e443346
clientesweb <alexisifran32@gmail.com>
2025-05-13 18:39:59 -0300

    Actualizar AddToCartBtn.tsx

--- src/components/product-page/Header/AddToCartBtn.tsx
+++ src/components/product-page/Header/AddToCartBtn.tsx
@@ -1,8 +1,7 @@
 "use client"
 
 import { addToCart } from "@/lib/features/carts/cartsSlice"
-import { useAppDispatch, useAppSelector } from "@/lib/hooks/redux"
-import type { RootState } from "@/lib/store"
+import { useAppDispatch } from "@/lib/hooks/redux"
 import type { Product } from "@/types/product.types"
 
 interface AddToCartBtnProps {
@@ -12,29 +11,28 @@ interface AddToCartBtnProps {
   price?: number
 }
 
-const AddToCartBtn = ({ data, selectedQuantity, selectedGeo }: AddToCartBtnProps) => {
+const AddToCartBtn = ({ data, selectedQuantity, selectedGeo, price }: AddToCartBtnProps) => {
   const dispatch = useAppDispatch()
-  const { sizeSelection, colorSelection } = useAppSelector((state: RootState) => state.products)
 
   return (
     <button
       type="button"
-      className="bg-black w-full ml-3 sm:ml-5 rounded-full h-11 md:h-[52px] text-sm sm:text-base text-white hover:bg-black/80 transition-all"
+      className="bg-black w-full rounded-full h-11 md:h-[52px] text-sm sm:text-base text-white hover:bg-black/80 transition-all"
       onClick={() =>
         dispatch(
           addToCart({
             id: data.id,
             name: data.title,
             srcUrl: data.srcUrl,
-            price: data.price,
-            attributes: [sizeSelection, colorSelection?.name, selectedQuantity, selectedGeo].filter(Boolean),
+            price: price || data.price,
+            attributes: [selectedQuantity, selectedGeo].filter((attr): attr is string => typeof attr === "string"),
             discount: data.discount,
-            quantity: 1, // Default quantity to 1 if not provided
+            quantity: 1, // Default quantity to 1
           }),
         )
       }
     >
-      Add to Cart
+      Agregar al Carrito
     </button>
   )
 }
