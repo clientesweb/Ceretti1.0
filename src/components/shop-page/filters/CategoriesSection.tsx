f5c3d65d115c93a8bca31f762429888a4ca97ee4
clientesweb <alexisifran32@gmail.com>
2025-05-13 13:58:15 -0300

    Actualizar CategoriesSection.tsx

--- src/components/shop-page/filters/CategoriesSection.tsx
+++ src/components/shop-page/filters/CategoriesSection.tsx
@@ -2,34 +2,51 @@ import Link from "next/link"
 import { MdKeyboardArrowRight } from "react-icons/md"
 
 type Category = {
+  id: number
   title: string
-  slug: string
+  value: string
 }
 
-const categoriesData: Category[] = [
+export const categories = [
   {
+    id: 1,
     title: "Instagram",
-    slug: "/shop?platform=instagram",
+    value: "instagram",
   },
   {
-    title: "Facebook",
-    slug: "/shop?platform=facebook",
-  },
-  {
-    title: "TikTok",
-    slug: "/shop?platform=tiktok",
-  },
-  {
+    id: 2,
     title: "YouTube",
-    slug: "/shop?platform=youtube",
+    value: "youtube",
   },
   {
-    title: "Twitter/X",
-    slug: "/shop?platform=twitter",
+    id: 3,
+    title: "TikTok",
+    value: "tiktok",
   },
   {
+    id: 4,
     title: "Spotify",
-    slug: "/shop?platform=spotify",
+    value: "spotify",
+  },
+  {
+    id: 5,
+    title: "Facebook",
+    value: "facebook",
+  },
+  {
+    id: 6,
+    title: "Threads",
+    value: "threads",
+  },
+  {
+    id: 7,
+    title: "SoundCloud",
+    value: "soundcloud",
+  },
+  {
+    id: 8,
+    title: "SEO",
+    value: "seo",
   },
 ]
 
@@ -37,8 +54,8 @@ const CategoriesSection = () => {
   return (
     <div className="flex flex-col space-y-0.5 text-black/60">
       <h3 className="text-black font-bold text-xl mb-4">Plataformas</h3>
-      {categoriesData.map((category, idx) => (
-        <Link key={idx} href={category.slug} className="flex items-center justify-between py-2">
+      {categories.map((category, idx) => (
+        <Link key={idx} href={`/shop?platform=${category.value}`} className="flex items-center justify-between py-2">
           {category.title} <MdKeyboardArrowRight />
         </Link>
       ))}
@@ -47,4 +64,3 @@ const CategoriesSection = () => {
 }
 
 export default CategoriesSection
-
