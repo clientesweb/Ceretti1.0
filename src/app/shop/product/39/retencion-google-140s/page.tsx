8f25570677b572d87f12c78fb7a162cb0b1b5ac5
clientesweb <alexisifran32@gmail.com>
2025-05-13 21:05:22 -0300

    Actualizar page.tsx

--- src/app/shop/product/39/retencion-google-140s/page.tsx
+++ src/app/shop/product/39/retencion-google-140s/page.tsx
@@ -8,7 +8,6 @@ import { relatedProductData } from "@/app/page"
 import { useState } from "react"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Checkbox } from "@/components/ui/checkbox"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 
